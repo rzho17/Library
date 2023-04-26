1097a381722c454a545e391c232534f7b31e8ca8
Richard <richardzehungho@gmail.com>
2023-04-26 11:00:21 -0700

    Refactor book constructor to use class

--- app.js
+++ app.js
@@ -94,11 +94,20 @@ formContainer.addEventListener("click", (e) => {
   }
 });
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 form.addEventListener("submit", (e) => {
